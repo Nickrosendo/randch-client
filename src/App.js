0cb67cff60916be021800f51b5d429e14407ee86
nicolas-rosendo <nicolasrosendo@hotmail.com>
2019-07-26 00:03:08 -0300

    conversation layout components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, TextArea, Comment, Icon } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+function Conversation() {
+  return (
+    <Comment.Group className="App-conversation">
+      <Comment className="CommentBox">
+        <Comment.Content>
+          <Comment.Author as="a">
+            <Icon name="user circle" />
+            Elliot Fu
+          </Comment.Author>
+          <Comment.Metadata>
+            <div>Yesterday at 12:30AM</div>
+          </Comment.Metadata>
+          <Comment.Text>
+            <p>This has been very useful for my research. Thanks as well!</p>
+          </Comment.Text>
+          <Comment.Actions>
+            <Comment.Action>Reply</Comment.Action>
+          </Comment.Actions>
+        </Comment.Content>
+      </Comment>
+    </Comment.Group>
+  );
+}
+
+function MessageBox() {
+  return (
+    <Form className="App-MessageBox">
+      <TextArea placeholder="Tell me more" />
+    </Form>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-body">
+      <Conversation />
+      <MessageBox />
     </div>
   );
 }
